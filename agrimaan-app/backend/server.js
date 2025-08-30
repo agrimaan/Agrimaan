7086c497a47735c0de94d7d0a94025706060c397
Agrimaan <agrimaan@github.com>
2025-08-30 07:41:37 +0000

    updated backend port

--- agrimaan-app/backend/server.js
+++ agrimaan-app/backend/server.js
@@ -52,7 +52,7 @@ const connectDB = async () => {
 };
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
